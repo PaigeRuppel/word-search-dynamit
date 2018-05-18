464c470cdc16e5ba763954e705f2d97d6a73414e
Paige Ruppel <paige.ruppel@gmail.com>
2018-05-17 20:34:49 -0400

    stream is making use of a bifunction to clean up words based on boundary

--- src/main/java/com/paigeruppel/interviews/dynamit/WordStreamReader.java
+++ src/main/java/com/paigeruppel/interviews/dynamit/WordStreamReader.java
@@ -1,15 +1,38 @@
 package com.paigeruppel.interviews.dynamit;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
-
-import static java.util.Arrays.asList;
 
 public class WordStreamReader {
 
-    public WordStreamReader(String filename) {
+    private final Path wordsFile;
+
+    public WordStreamReader(String filename) throws URISyntaxException {
+        wordsFile = Paths.get(getClass().getClassLoader().getResource(filename).toURI());
     }
 
-    public Stream<String> stream() {
-        return asList("clean", "words").stream();
+    public Stream<String> stream() throws IOException {
+        Function<String, Collection<String>> wordExtractor = new Function<String, Collection<String>>() {
+            @Override
+            public Collection<String> apply(String line) {
+                Matcher wordBoundaryMatcher = Pattern.compile("([\\w]+)").matcher(line);
+
+                Collection<String> words = new ArrayList<>();
+                while(wordBoundaryMatcher.find()) {
+                    words.add(wordBoundaryMatcher.group());
+                }
+                return words;
+            }
+        };
+        return wordExtractor.apply(Files.lines(wordsFile).findFirst().get()).stream();
     }
 }
--- src/test/java/com/paigeruppel/interviews/dynamit/WordStreamReaderTest.java
+++ src/test/java/com/paigeruppel/interviews/dynamit/WordStreamReaderTest.java
@@ -2,21 +2,29 @@ package com.paigeruppel.interviews.dynamit;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class WordStreamReaderTest {
 
     @Test
-    public void shouldReadCleanWords() {
+    public void shouldReadCleanWords() throws URISyntaxException, IOException {
         WordStreamReader underTest = new WordStreamReader("CleanWords.txt");
-
         Stream<String> result = underTest.stream();
-
         assertThat(result.collect(toList()), contains("clean", "words"));
+    }
+
+    @Test
+    public void shouldIgnoreTrailingPunctuation() throws URISyntaxException, IOException {
+        WordStreamReader underTest = new WordStreamReader("TrailingPunctuation.txt");
+        Stream<String> result = underTest.stream();
+        assertThat(result.collect(toList()), contains("comma", "word", "quotation"));
     }
 }
